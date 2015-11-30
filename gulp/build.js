630d1f080f15a3033cadff0448ba650a4b3856fd
kinogam <kino@igola.com>
2015-11-30 17:23:56 +0800

    update dev env

---
+++ gulp/build.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var conf = require('./conf');
+
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
+});
+
+gulp.task('build', ['scripts'], function () {
+
+  return gulp.src(path.join(conf.paths.tmp, '/ng-rules.js'))
+    .pipe($.sourcemaps.init())
+    .pipe($.uglify({ preserveComments: $.uglifySaveLicense })).on('error', conf.errorHandler('Uglify'))
+    .pipe($.sourcemaps.write('maps'))
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/')))
+    .pipe($.size({ title: path.join(conf.paths.dist, '/'), showFiles: true }));
+  });
+
+
+gulp.task('clean', function () {
+  return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
+});
