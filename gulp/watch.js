69c4f2701bc09c106c76191dfdbb9ef959adf7ac
kinogam <kino@igola.com>
2015-12-25 15:37:40 +0800

    update gulp watch

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,27 +4,4 @@ var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
 
-function isOnlyChange(event) {
-  return event.type === 'changed';
-}
-
-gulp.task('watch', ['scripts:watch', 'inject'], function () {
-
-/*  gulp.watch([path.join(conf.paths.src, '/!*.html'), 'bower.json'], ['inject']);
-
-  gulp.watch([
-    path.join(conf.paths.src, '/app/!**!/!*.css'),
-    path.join(conf.paths.src, '/app/!**!/!*.less')
-  ], function(event) {
-    if(isOnlyChange(event)) {
-      gulp.start('styles');
-    } else {
-      gulp.start('inject');
-    }
-  });
-
-
-  gulp.watch(path.join(conf.paths.src, '/app/!**!/!*.html'), function(event) {
-    browserSync.reload(event.path);
-  });*/
-});
+gulp.task('watch', ['scripts:watch']);
