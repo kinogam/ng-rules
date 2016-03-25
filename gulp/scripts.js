2f5da5cff9340f903f7b65e89e2b2a229eca0034
kinogam <kino@igola.com>
2016-03-25 15:21:52 +0800

    update npm packages and webpack config

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -9,60 +9,74 @@ var webpack = require('webpack-stream');
 var $ = require('gulp-load-plugins')();
 
 function webpackWrapper(watch, test, callback) {
-  var webpackOptions = {
-    watch: watch,
-    module: {
-      preLoaders: [{ test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader'}],
-      loaders: [{ test: /\.js$/, exclude: /node_modules/, loaders: ['ng-annotate', 'babel-loader']}]
-    },
-    output: { filename: 'ng-rules.js' }
-  };
+    var webpackOptions = {
+        watch: watch,
+        module: {
+            preLoaders: [{test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader'}],
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: 'babel',
+                    query: {
+                        presets: ['es2015']
+                    }
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'ng-annotate'
+                }
+            ]
+        },
+        output: {filename: 'ng-rules.js'}
+    };
 
-  if(watch || test) {
-    webpackOptions.devtool = 'inline-source-map';
-  }
-
-  var webpackChangeHandler = function(err, stats) {
-    if(err) {
-      conf.errorHandler('Webpack')(err);
+    if (watch || test) {
+        webpackOptions.devtool = 'inline-source-map';
     }
 
-    $.util.log(stats.toString({
-      colors: $.util.colors.supportsColor,
-      chunks: false,
-      hash: false,
-      version: false
-    }));
+    var webpackChangeHandler = function (err, stats) {
+        if (err) {
+            conf.errorHandler('Webpack')(err);
+        }
+
+        $.util.log(stats.toString({
+            colors: $.util.colors.supportsColor,
+            chunks: false,
+            hash: false,
+            version: false
+        }));
 
 
-    if(watch) {
-      watch = false;
-      callback();
+        if (watch) {
+            watch = false;
+            callback();
+        }
+    };
+
+    var sources = [path.join(conf.paths.src, '/ng-rules.js')];
+    if (test) {
+        sources.push(path.join(conf.paths.src, '/**/*.spec.js'));
     }
-  };
 
-  var sources = [ path.join(conf.paths.src, '/ng-rules.js') ];
-  if (test) {
-    sources.push(path.join(conf.paths.src, '/**/*.spec.js'));
-  }
-
-  return gulp.src(sources)
-    .pipe(webpack(webpackOptions, null, webpackChangeHandler))
-    .pipe(gulp.dest(path.join(conf.paths.tmp, '/')));
+    return gulp.src(sources)
+        .pipe(webpack(webpackOptions, null, webpackChangeHandler))
+        .pipe(gulp.dest(path.join(conf.paths.tmp, '/')));
 }
 
 gulp.task('scripts', function () {
-  return webpackWrapper(false, false);
+    return webpackWrapper(false, false);
 });
 
 gulp.task('scripts:watch', ['scripts'], function (callback) {
-  return webpackWrapper(true, false, callback);
+    return webpackWrapper(true, false, callback);
 });
 
 gulp.task('scripts:test', function () {
-  return webpackWrapper(false, true);
+    return webpackWrapper(false, true);
 });
 
 gulp.task('scripts:test-watch', ['scripts'], function (callback) {
-  return webpackWrapper(true, true, callback);
+    return webpackWrapper(true, true, callback);
 });
