1b4d3b913f7c32bff0ae51b7833939425d1da0fe
kinogam <kino@igola.com>
2016-05-13 11:29:42 +0800

    seperate test

--- src/ng-rules.spec.js
+++ src/ng-rules.spec.js
@@ -617,32 +617,3 @@ describe('ng-rules', () => {
 });
 
 
-
-
-import pseudoFilter from  './pseudo-filter';
-
-describe('pseudo-filter', () => {
-    it('first child', () => {
-        var fn = pseudoFilter(':first-child');
-
-        expect(fn(0)).toBe(true);
-
-        expect(fn(1)).toBe(false);
-    });
-
-    it('reverse', () => {
-        var fn = pseudoFilter(':not(:first-child)');
-        expect(fn(1)).toBe(true);
-    });
-
-
-    it('nth-child', () => {
-        var fn = pseudoFilter(':nth-child(3)');
-
-        expect(fn(2)).toBe(true);
-
-        expect(fn(3)).toBe(false);
-    });
-});
-
-
